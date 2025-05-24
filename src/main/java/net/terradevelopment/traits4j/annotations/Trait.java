4eae8e3a14a8189b9de7d4dfe7607a1bad6530b5
SebaSphere <SebaSphereEmails@gmail.com>
2025-05-24 15:01:45 -0400

    chore: runtime annotation

--- src/main/java/net/terradevelopment/traits4j/annotations/Trait.java
+++ src/main/java/net/terradevelopment/traits4j/annotations/Trait.java
@@ -1,9 +1,12 @@
 package net.terradevelopment.traits4j.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.ArrayList;
 
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Trait {
 
